904d93f333212784049d478f98d6fa74376a6635
zero88 <sontt246@gmail.com>
2022-03-08 21:59:33 +0700

    [rsql] Tweak jpa-ext dependency in CI build and local build

--- rsql/core/build.gradle.kts
+++ rsql/core/build.gradle.kts
@@ -1,5 +1,8 @@
+val profile: String by project.ext
+val jpaLib = "io.github.zero88:jpa-ext:1.1.0-SNAPSHOT" // on CI system. Version 1.1.0-SNAPSHOT is temporary
+
 dependencies {
-    api(project(":jpa-ext"))
+    api(if (profile == "rsql") jpaLib else project(":jpa-ext"))
     api(LogLibs.slf4j)
     api(ZeroLibs.utils)
     api(ZeroLibs.rsql)
