e09a49201cce01f533e0761566fa5f6559f0f3d3
zero88 <sontt246@gmail.com>
2022-06-17 19:16:55 +0700

    [gradle] Invalidate cache when changed schema file in resource folder [gradle] Refactor init test container to generate SQL schema

--- buildSrc/src/main/kotlin/JooqTestContainer.kt
+++ buildSrc/src/main/kotlin/JooqTestContainer.kt
@@ -1,9 +1,27 @@
 data class TestContainerJDBC(val driver: String, val url: String)
 
-fun getTestContainer(dbVersion: String, schemaFile: String, dbUser: String? = null): TestContainerJDBC {
-    val param = if (dbUser.isNullOrBlank()) "" else "user=${dbUser}&"
+fun getTestContainer(dbVersion: String, schemaFile: String, params: Map<String, Any> = mapOf()): TestContainerJDBC =
+    getTestContainer(dbVersion, params.plus("TC_INITSCRIPT" to "file:${schemaFile}"))
+
+fun getTestContainerByFun(
+    dbVersion: String,
+    functionName: String,
+    params: Map<String, Any> = mapOf()
+): TestContainerJDBC = getTestContainer(dbVersion, params.plus("TC_INITFUNCTION" to functionName))
+
+fun getTestContainer(dbVersion: String, params: Map<String, Any> = mapOf()): TestContainerJDBC {
+    val param = optimizeDockerParams().plus(params).map { "${it.key}=${it.value}" }.joinToString("&")
     return TestContainerJDBC(
         driver = "org.testcontainers.jdbc.ContainerDatabaseDriver",
-        url = "jdbc:tc:${dbVersion}:///jooqx?${param}TC_TMPFS=/testtmpfs:rw&TC_INITSCRIPT=file:${schemaFile}"
+        url = "jdbc:tc:${dbVersion}:///jooqx?${param}"
     )
+}
+
+private fun optimizeDockerParams(): Map<String, Any> {
+    val os = System.getProperty("os.name").toLowerCase()
+    return when {
+        // https://docs.docker.com/storage/tmpfs/
+        os.contains("nix") || os.contains("nux") || os.contains("aix") -> mapOf("TC_TMPFS" to "/testtmpfs:rw")
+        else                                                           -> mapOf()
+    }
 }
