71ef540628ddeb872d5d5575ee3fc83663cf78c3
zero88 <sontt246@gmail.com>
2024-04-20 17:13:32 +0700

    feat(gradle): use version catalogs in buildSrc

---
+++ buildSrc/settings.gradle.kts
@@ -0,0 +1,10 @@
+@file:Suppress("UnstableApiUsage")
+
+// https://github.com/gradle/gradle/issues/15383
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
